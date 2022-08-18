102611c8bd387f919fbb6c3b10071e1ae84858d5
negar miralaie <miralaienegar@gmail.com>
2022-08-18 16:08:31 +0430

    update

--- config/database.js
+++ config/database.js
@@ -1,4 +1,8 @@
 const Sequelize = require('sequelize');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 // To connect to the database that we made in mysql workbench, We need to create a new instance of sequelize and
 // pass in the database name of db, username, and password, options object.
 // For creating an instance of Sequelize we should say: new Constructor -> Constructor is Sequelize
--- index.js
+++ index.js
@@ -11,25 +11,11 @@ const database = require('./config/database');
 const testDatabaseConnection = require('./config/test_database_connection');
 testDatabaseConnection();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // useres routes
 app.use('/users', require('./routes/users'));
-// // Model synchronization: Inserts a table that you defined with sequelize into your database.
-// // Sync is another way of writing sql
-// // Note that sync method creates a table only if it does not exist. WE can turn it off by passing force: true -> Drops previous table and creates a new one
-// // Or by giving alter: true rather than force: true, It won't drop the table
-// User.sync({ force: true }).then(() => {
-//     console.log('Table and model is ssynced successfully.');
-// }).catch((error) => {
-//     console.log('Error syncing the table and model.');
-// },
-// {
-//     // Note that sequelize automatically plurizations name to be Users so Users will be the name of the table.
-//     // B setting freezeTableName to true, we say we do not want the name of the table to be pluralized.
-//     freezeTableName: true,
-//     // Sequelize automatically adds createdAt and updatedAT.
-//     // WE can turn that off using timestamps: false
-//     timestamps: false,
-// });
 
 app.get('/', (req, res) => {
    res,rend('Home page~'); 
