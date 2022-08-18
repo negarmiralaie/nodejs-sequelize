d34b047b95d2f291351d039002c3fd6086a2fe69
negar miralaie <miralaienegar@gmail.com>
2022-08-19 03:28:37 +0430

    update

--- routes/users.js
+++ routes/users.js
@@ -3,17 +3,49 @@ const router = express.Router();
 const database = require('../config/database');
 const UserModel = require('../models/User');
 const createError = require('http-errors');
+const { Op } = require('sequelize');
 
 router.get('/get-all', async (req, res) => {
    try{
-      // findAll returns an array 
-      const allUsers = await UserModel.findAll();
-      // We can set attributes to get only certain columns
-      // const allUsers = await UserModel.findAll({ attributes: ['username', 'password'] });
-      // When we want to return username as myName and password as pwd: -> AS
-      // const allUsers = await UserModel.findAll({ attributes: [['username', 'myName'], ['password', 'pwd']] });
-      // Aggregation -> sequelize.fn
-      const allUsers = await UserModel.findAll({ attributes: [database.fn()] });
+        // findAll returns an array 
+        const allUsers = await UserModel.findAll();
+        // We can set attributes to get only certain columns
+        // const allUsers = await UserModel.findAll({ attributes: ['username', 'password'] });
+        // When we want to return username as myName and password as pwd: -> AS
+        // const allUsers = await UserModel.findAll({ attributes: [['username', 'myName'], ['password', 'pwd']] });
+        // Aggregation -> sequelize.fn
+        //const allUsers = await UserModel.findAll({ attributes: [[database.fn('SUM', database.col('age')), 'howOld']]});
+        //We want to get every field except the password
+        //const allUsers = await UserModel.findAll({ attributes: {exclude: ['password']}});
+        //where is used to filter the query based on certain conditions
+        //here we want to get everything where the age is 45
+        //const allUsers = await UserModel.findAll({ where: { age: 45 } });
+        // Get username where age is 45
+        //const allUsers = await UserModel.findAll({ attributes: ['username'], where: { age: 45 } });
+        //const allUsers = await UserModel.findAll({ where: { age: 45, username: John } });
+        //limit is used to limit the number of rows returned
+        //const allUsers = await UserModel.findAll({ limit: 2 });
+        // order by is used to sort the results in descending or ascending order
+        // order by ages desecnding
+        //   const allUsers = await UserModel.findAll({ order: [['age', 'DESC']] });
+        // group by is used to group rows that have same values into summery rows, it works similar to order except that it does not take direction as the last argument(ASC or DESC)
+        //  It takes a string directly to the group key, group key is mostly used with aggregate functions
+        // Here we want to add all the ages of the usernames that are the same -> we will get a username and the sum of the ages with that username
+        // const allUsers = await UserModel.findAll({ attribute : ['username', 
+        //         [database.fn('SUM', database.col('age')), 'sum_age']], group: ['username'] });
+        // Operators in sequelized are used by op
+        // return results that age is 45 or username is John
+        // const allUsers = await UserModel.findAll({ where: {[Op.or]: {username: 'John', age: 45}} });
+        // const allUsers = await UserModel.findAll({ where: {username: 'John', age: 45}});
+        // const allUsers = await UserModel.findAll({ where: {[Op.and]: {username: 'John', age: 45}} });
+        // We can get the same result without using Op.and
+        // const allUsers = await UserModel.findAll({ where: {username: 'John', age: 45}});
+        // Al users that their age is greater than 45
+        // const allUsers = await UserModel.findAll({ where: {age: {[Op.gt]: 45}} });
+        // All users that their age is less than 45 or their age is null
+        // const allUsers = await UserModel.findAll({ where: {age: {[Op.or]: { [Op.lt]: 45, [Op.eq]: null}}} });
+        // All users that their username's length is 6
+        // const allUsers = await UserModel.findAll({ where: database.where(database.fn('char_length', database.col('username')), 6)  });
 
         console.log('allUsers', allUsers);
         return res.json(allUsers);
